edc0ba22b13a33e54a427d278298318cb6f94c47
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-13 12:16:47 +0400

    mysql crate connection

---
+++ src/utils/mysql/entity.ts
@@ -0,0 +1,5 @@
+import { Author } from 'src/author/author.entity';
+import { Book } from 'src/book/book.entity';
+import { Genre } from 'src/genre/genre.entity';
+
+export const Entities = [Author, Book, Genre];

8530960f28d4deca8b2967db7fbbc19da7e0cafe
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-13 12:50:07 +0400

    create seed

---
+++ src/utils/mysql/seed/genre.ts
@@ -0,0 +1,62 @@
+export const Genre = [
+  {
+    title: 'Classics',
+  },
+  {
+    title: 'Tragedy',
+  },
+  {
+    title: 'Sci-Fi',
+  },
+  {
+    title: 'Fantasy',
+  },
+  {
+    title: 'Action and Adventure',
+  },
+  {
+    title: 'Crime & Mystery',
+  },
+  {
+    title: 'Romance',
+  },
+  {
+    title: 'Humor and Satire',
+  },
+  {
+    title: 'Horror',
+  },
+  {
+    title: 'Comics',
+  },
+  {
+    title: 'Memoirs',
+  },
+  {
+    title: 'Cookbooks',
+  },
+  {
+    title: 'True Stories',
+  },
+  {
+    title: 'War',
+  },
+  {
+    title: 'Documentary',
+  },
+  {
+    title: 'Adult',
+  },
+  {
+    title: 'Western',
+  },
+  {
+    title: 'Romance',
+  },
+  {
+    title: 'Fantasy',
+  },
+  {
+    title: 'Religion',
+  },
+];

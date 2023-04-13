f2998746ecd225a5539e8f5ae8beee6ab4d93118
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-13 15:29:53 +0400

    create entity

--- src/utils/mysql/entity.ts
+++ src/utils/mysql/entity.ts
@@ -1,5 +1,5 @@
+import { GenreEntity } from 'src/genre/genre.entity';
 import { Author } from 'src/author/author.entity';
-import { Genre } from 'src/genre/genre.entity';
 import { Book } from 'src/book/book.entity';
 
-export const Entities = [Author, Book, Genre];
+export const Entities = [Author, Book, GenreEntity];

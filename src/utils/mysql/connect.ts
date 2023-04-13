040babec2737de16c08369bd4c92b554bc25723c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-13 13:25:40 +0400

    update connection

--- src/utils/mysql/connect.ts
+++ src/utils/mysql/connect.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Entities } from './entity';
 
-export const OrmConnection = TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: process.env.host,
-  port: Number(process.env.port),
-  username: process.env.username,
-  password: process.env.password,
-  database: process.env.database,
-  entities: Entities,
-  synchronize: true,
+export const OrmConnection = TypeOrmModule.forRootAsync({
+  useFactory: async () => ({
+    type: 'mysql',
+    host: process.env.host,
+    port: Number(process.env.port),
+    username: process.env.username,
+    password: process.env.password,
+    database: process.env.database,
+    entities: Entities,
+    synchronize: true,
+  }),
 });
--- src/utils/mysql/entity.ts
+++ src/utils/mysql/entity.ts
@@ -1,5 +1,5 @@
 import { Author } from 'src/author/author.entity';
-import { Book } from 'src/book/book.entity';
 import { Genre } from 'src/genre/genre.entity';
+import { Book } from 'src/book/book.entity';
 
 export const Entities = [Author, Book, Genre];
